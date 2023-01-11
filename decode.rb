6317d115e011fdcf799e4ae11f3c3d3a80c90cc8
SaboorSohaib <abdulsaboorsohaib.com>
2023-01-11 15:14:04 +0430

    fix linter

--- decode.rb
+++ decode.rb
@@ -1,65 +1,64 @@
 @character = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z',
-    '.----' => '1',
-    '..---' => '2',
-    '...--' => '3',
-    '....-' => '4',
-    '.....' => '5',
-    '-....' => '6',
-    '--...' => '7',
-    '---..' => '8',
-    '----.' => '9',
-    '-----' => '0'
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0'
 }
 
-def getChar(char)
-    @character[char]
+def get_char(char)
+  @character[char]
 end
-puts getChar(".-")
-    
-def getWord(words)
-    morseWord = words.split
-    word = ''
-    morseWord.each do |char|
-        word += getChar(char)
-    end
-    word
-end
-puts getWord("-- -.--")
+puts get_char('.-')
 
-def getSentence(bottole_sentence)
-    morseSentence = bottole_sentence.split(' ')
-    sentence = ''
-    morseSentence.each do |char|
-        sentence += getWord(char)
-        sentence += ''
-    end
-    sentence
+def get_word(words)
+  morse_word = words.split
+  word = ''
+  morse_word.each do |char|
+    word += getChar(char)
+  end
+  word
 end
-puts getSentence(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts get_word('-- -.--')
 
+def get_sentence(bottole_sentence)
+  morse_sentence = bottole_sentence.split
+  sentence = ''
+  morse_sentence.each do |char|
+    sentence += get_Word(char)
+    sentence += ''
+  end
+  sentence
+end
+puts get_sentence(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
